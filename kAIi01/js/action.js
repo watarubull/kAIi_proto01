0e00b371e8dcca66ab5999a4401a47a8785dc078
watarubull <87561644+watarubull@users.noreply.github.com>
2021-10-23 13:05:19 +0900

    Add files via upload

--- kAIi01/js/action.js
+++ kAIi01/js/action.js
@@ -6,3 +6,17 @@ $(".kaii-obj .walk").on("click",function(){
 $(".close").on("click",function(){
   $(".kaii-info").fadeToggle(200);
 });
+
+$(".add-btn,.close-btn").on("click",function(){
+  $(".post-area").toggleClass("open");
+});
+
+function postEpisode(){
+  $(".result-post-area").fadeIn(400);
+}
+
+$(".result-post-area").on("click",function(){
+  $(".result-post-area").fadeOut(400);
+  $(".post-area").toggleClass("open");
+  $(".kaii-obj").fadeIn(200);
+});
